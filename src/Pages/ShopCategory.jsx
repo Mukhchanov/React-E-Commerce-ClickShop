9b8a806a90b60fb5279387cc215feb4cf753ff8c
Mukhchanov <parviz.mukhchanov@gmail.com>
2024-03-02 21:32:07 +0400

    Added React context for managing shop-related data and rendered product items based on category

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import '../Styles/Layouts/_shopcategory.scss';
+import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from '../Assets/dropdown_icon.png';
+import Item from '../Components/Item';
 
-function ShopCategory() {
+function ShopCategory(props) {
+
+    const {all_product} = useContext(ShopContext);
+
     return (
-        <div>
-            
+        <div className='shop-category'>
+            <img className='shop-category__banner' src={props.banner} />
+            <div className="shop-category-indexSort">
+                <p>
+                    <span>Showing 1-12</span> out of 36 products
+                </p>
+                <div className="shop-category-sort">
+                    Sort by <img src={dropdown_icon} alt="" />
+                </div>
+            </div>
+            <div className="shop-category-products container">
+                {all_product.map((item, i) => {
+                    if(props.category === item.category) {
+                        return <Item key={i} 
+                        id={item.id} 
+                        name={item.name} 
+                        image={item.image} 
+                        new_price={item.new_price} 
+                        old_price={item.old_price} />
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
+            <div className="shop-category-loadmore">
+                Explore More
+            </div>
         </div>
     )
 };
