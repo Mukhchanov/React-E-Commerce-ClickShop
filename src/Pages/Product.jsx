6dc8b9bda007df55b1d02171483a7853393e56d7
Mukhchanov <parviz.mukhchanov@gmail.com>
2024-03-30 11:44:10 +0400

    Updated media queries for improved mobile layout

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrum';
+import '../Styles/Layouts/_product.scss';
 import ProductDisplay from '../Components/ProductDisplay';
 import DescriptionBox from '../Components/DescriptionBox';
 import RelatedProducts from '../Components/RelatedProducts';
@@ -13,7 +14,7 @@ function Product() {
     const product = all_product.find((e) => e.id === Number(productId));
 
     return (
-        <div className='product container-fluid'>
+        <div className='product container'>
             {product && <Breadcrum product={product} />}
             {product && <ProductDisplay product={product} />}
             <DescriptionBox />
